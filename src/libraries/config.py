ca55a44685abe9f6d5d1ecfbefecfd4828861768
Rinfair <contact@rinfair.moe>
2023-02-18 21:18:06 +0800

    Change API to official
    Signed-off-by: Rinfair <contact@rinfair.moe>

--- src/libraries/config.py
+++ src/libraries/config.py
@@ -3,11 +3,14 @@ class Config:
     bot_id = 'Your Bot QQ'
     # Bot的超级管理员
     superuser = ['Super User QQ']
-    # ChatGPT的API key（请在 https://platform.openai.com/account/api-keys 申请）
-    openai_api_key = 'Your OpenAI API Key'
-    # ChatGPT的语言模型引擎
-    openai_engine = 'text-davinci-003'
-    # ChatGPT的语言自由度参数
-    openai_temperature = 1
-    # ChatGPT的最大生成长度
-    openai_tokens = 1024
+    # ChatGPT的认证选项（1为使用账号密码，2为使用session_token）
+    # 若选择了其中一个，则无需填写其他选项的对应配置
+    openai_authentication = 1
+    # 1-ChatGPT的账号（邮箱）
+    openai_email = 'Your OpenAI Account Email'
+    # 1-ChatGPT的密码
+    openai_password = 'Password'
+    # ChatGPT使用的代理（请务必修改为自己的代理！请确保该代理可以正常使用chat.openai.com的ChatGPT）
+    openai_proxy = 'localhost'
+    # 2-ChatGPT的session_token
+    openai_session_token = 'Session Token'
